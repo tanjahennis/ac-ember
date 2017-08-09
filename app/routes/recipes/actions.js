c5f47341dbfeee20d71a45d8d819fe5e85c0cd6e
Tanja Hennis <tmhennis@gmail.com>
2017-08-09 16:26:02 +0200

    remove ingredient; remove instruction

--- app/routes/recipes/actions.js
+++ app/routes/recipes/actions.js
@@ -18,8 +18,18 @@ export default Ember.Route.extend({
       this.currentModel.get('ingredients').pushObject(this.get('store').createRecord('ingredient'));
     },
 
+    removeIngredient(model, ingredient) {
+      model.get('ingredients').removeObject(ingredient);
+      model.save();
+    },
+
     addInstruction() {
       this.currentModel.get('instructions').pushObject(this.get('store').createRecord('instruction'));
+    },
+
+    removeInstruction(model, instruction) {
+      model.get('instructions').removeObject(instruction);
+      model.save();
     },
   }
 });
